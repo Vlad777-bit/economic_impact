9f79bbc2913b2eeb38115b3d9832a96d6937fd62
Vladislav Nozhkin <VNozhkin@exolve.ru>
2025-09-15 17:40:08 +0300

    init spring-boot

---
+++ src/main/java/ru/utmn/nozhkin/exposure/ExposureApplication.java
@@ -0,0 +1,11 @@
+package ru.utmn.nozhkin.exposure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExposureApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExposureApplication.class, args);
+	}
+}
