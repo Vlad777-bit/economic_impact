1c3c1cf7af6fe99741d5cc25f9767a9c533e89a0
Vladislav Nozhkin <VNozhkin@exolve.ru>
2025-09-22 15:37:13 +0300

    add controller

---
+++ src/main/java/ru/utmn/nozhkin/exposure/controller/ExposureApplicationController.java
@@ -0,0 +1,12 @@
+package ru.utmn.nozhkin.exposure.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExposureApplicationController {
+    @GetMapping("/project-name")
+    public String getProjectName() {
+        return "Exposure";
+    }
+}
